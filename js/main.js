1be00fe5887c249d7769e23a4631f51833b59214
shazzadhossen <king.voip123@gmail.com>
2020-10-22 12:36:32 +0600

    commit here

--- js/main.js
+++ js/main.js
@@ -24,3 +24,71 @@ $(document).ready(function(){
 });
 
          /***** owl.carousel*****/ 
+
+       //initiating jQuery
+jQuery(function($) {
+         $(document).ready( function() {
+         //enabling stickUp on the '.navbar-wrapper' class
+         $('.navbar-wrapper').stickUp();
+     });
+  });
+
+jQuery(function($) {
+         $(document).ready( function() {
+         //enabling stickUp on the '.navbar-wrapper' class
+         new WOW().init();
+     });
+  });
+ 
+jQuery(function($) {
+         $(document).ready( function() {
+         //enabling stickUp on the '.navbar-wrapper' class
+        $('.demo').animationCounter({
+
+          start: 0,
+          end: 1500,
+          delay: 100
+
+      });
+      $('.code').animationCounter({
+
+          start: 0,
+          end: 1000,
+          delay: 10
+
+      });
+      $('.bicycle').animationCounter({
+
+          start: 0,
+          end: 1500,
+          delay: 500
+
+      });
+      $('.coffee').animationCounter({
+
+          start: 0,
+          end: 1000,
+          delay: 100
+
+      });
+     });
+  });
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
